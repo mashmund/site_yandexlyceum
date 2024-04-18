58c81d4d34de26e2a1b936cdfa2ef18b6f5eff24
supersobaka <fstar08@bk.ru>
2024-04-18 16:37:14 +0300

    well x3

--- data/forms/add_jobs.py
+++ data/forms/add_jobs.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, SubmitField, SelectField
+from wtforms import StringField, IntegerField, BooleanField, SubmitField, SelectField, FileField
 from wtforms.validators import DataRequired
 
 
@@ -10,4 +10,6 @@ class JobForm(FlaskForm):
     spher = StringField('Сфера', validators=[DataRequired()])
     age = SelectField('Возраст', choices=['14', '16', '18'], validators=[DataRequired()])
     adress = StringField('Адрес проведения', validators=[DataRequired()])
+    contact = StringField('Номер телефона', validators=[DataRequired()])
+    photo = FileField('Фото', validators=[DataRequired()])
     submit = SubmitField('Добавить')
